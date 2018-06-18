988c1a23957edc80121ab4e8e9b3c2b2af3995d1
Stuart Cameron <Admin@stuarts-macbook-pro-2.local>
2018-06-18 00:06:00 -0400

    done with graphs and table

--- playing with sql/Functions.swift
+++ playing with sql/Functions.swift
@@ -1,7 +1,9 @@
 import SQLite3
+import UIKit
+import Charts
 
 class Functions {
-    func get_all_from_table(db: OpaquePointer) {
+    func get_all_from_table(db: OpaquePointer) -> [Entry] {
         var stmt: OpaquePointer?
         
         var entry_list = [Entry]()
@@ -9,7 +11,7 @@ class Functions {
         
         if sqlite3_prepare(db, query, -1, &stmt, nil) != SQLITE_OK{
             print("didnt prep")
-            return
+            return entry_list
         }
         
         while(sqlite3_step(stmt) == SQLITE_ROW){
@@ -20,6 +22,99 @@ class Functions {
             entry_list.append(Entry(entry: Int(entry), km: Int(km), gas: Double(gas)))
             
             print("entry: ", entry, " km: ", km , " gas: ", gas)
+        }
+        return entry_list
+    }
+    
+    func add_gas(db : OpaquePointer, km_input: UITextField, gas_input: UITextField ) {
+        var add: OpaquePointer?
+        
+        let add_query = "INSERT INTO gas_table (km, gas) VALUES (?,?)"
+        
+        if sqlite3_prepare(db, add_query, -1, &add, nil) != SQLITE_OK{
+            debugPrint("couldnt prep")
+            return
+        }
+        
+        if sqlite3_bind_int(add, 1, Int32(km_input.text!)!) != SQLITE_OK{
+            debugPrint("could bind km")
+            return
+        }
+        
+        if sqlite3_bind_double(add, 2, Double(gas_input.text!)!) != SQLITE_OK{
+            debugPrint("could bind gas")
+            return
+        }
+        
+        if sqlite3_step(add) != SQLITE_DONE{
+            print("couldnt added")
+            return
+        }else{
+            debugPrint("added")
+        }
+    }
+    
+    func set_chart(lineChartView: LineChartView){
+        let values_entry = [13,45,33,67,7,28,85,6,10,18,22,16,13,24,44,88]
+        let size = values_entry.count
+        
+        
+        
+        let values = (0...(size-1)).map { (i) -> ChartDataEntry in
+            return ChartDataEntry(x: Double(i), y: Double(values_entry[i]))
+        }
+        
+        let set1 = LineChartDataSet(values: values, label: "DataSet 1")
+        let data = LineChartData(dataSet: set1)
+        
+        lineChartView.data = data
+    }
+    
+    func delete_table(db: OpaquePointer) -> String {
+        
+        var delete: OpaquePointer?
+        let delte_query = "DROP TABLE km_database.km"
+        
+        
+        
+        if sqlite3_prepare(db, delte_query, -1, &delete, nil) != SQLITE_OK{
+            debugPrint("couldnt prep")
+            return "failed :("
+        }
+        
+        if sqlite3_step(delete) != SQLITE_DONE {
+            print("didnt delte")
+            return "didnt delte"
+        }
+        
+        return "worked"
+    }
+    
+    func add_to_empty(db: OpaquePointer) {
+        var add: OpaquePointer?
+        
+        let add_query = "INSERT INTO gas_table (km, gas) VALUES (?,?)"
+        
+        if sqlite3_prepare(db, add_query, -1, &add, nil) != SQLITE_OK{
+            debugPrint("couldnt prep")
+            return
+        }
+        
+        if sqlite3_bind_int(add, 1, 0) != SQLITE_OK{
+            debugPrint("could bind km")
+            return
+        }
+        
+        if sqlite3_bind_double(add, 2, Double(0)) != SQLITE_OK{
+            debugPrint("could bind gas")
+            return
+        }
+        
+        if sqlite3_step(add) != SQLITE_DONE{
+            print("couldnt added")
+            return
+        }else{
+            debugPrint("added")
         }
     }
 }
--- playing with sql/ViewController.swift
+++ playing with sql/ViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 import SQLite3
+import Charts
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
     
+    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var km_input: UITextField!
+    @IBOutlet weak var gas_input: UITextField!
     
     var db: OpaquePointer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-        // Do any additional setup after loading the view, typically from a nib.
+        
       
         let dirctery = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask,
                                                        appropriateFor: nil, create: false).appendingPathComponent("km_database.sqlite")
@@ -29,57 +31,27 @@ class ViewController: UIViewController, UITextFieldDelegate {
             print("error loading db")
         }
         
-        let createTable = "CREATE TABLE IF NOT EXISTS km (entry INTEGER PRIMARY KEY AUTOINCREMENT, km INTEGER, gas REAL)"
+        let createTable = "CREATE TABLE IF NOT EXISTS gas_table (entry INTEGER PRIMARY KEY AUTOINCREMENT, km INTEGER, gas REAL)"
         
         if sqlite3_exec(db, createTable, nil, nil, nil) != SQLITE_OK{
             print("couldnt create table")
         }
         
+        print(Functions().delete_table(db: db!))
+        
         debugPrint("everything worked !!!")
+        if lineChartView != nil{
+            Functions().set_chart(lineChartView: lineChartView)
+        }
     }
     
-    @IBOutlet weak var km_input: UITextField!
-    @IBOutlet weak var gas_input: UITextField!
-    
-    
-    
-    @IBAction func add_info(_ sender: Any) {
-        var add: OpaquePointer?
-        
-        let add_query = "INSERT INTO km (km, gas) VALUES (?,?)"
-        
-        if sqlite3_prepare(db, add_query, -1, &add, nil) != SQLITE_OK{
-            debugPrint("couldnt prep")
-            return
-        }
-        
-        if sqlite3_bind_int(add, 1, Int32(km_input.text!)!) != SQLITE_OK{
-            debugPrint("could bind km")
-            return
-        }
-        
-        if sqlite3_bind_double(add, 2, Double(gas_input.text!)!) != SQLITE_OK{
-            debugPrint("could bind gas")
-            return
-        }
-        
-        if sqlite3_step(add) != SQLITE_DONE{
-            print("couldnt added")
-            return
-        }else{
-            debugPrint("added")
-        }
-        
-        print()
-        print()
-        
-        Functions().get_all_from_table(db: db!)
-        
-    }
+
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
             self.view.endEditing(true)
     }
-    
+    @IBAction func enter_gas() {
+        Functions().add_gas(db: db!, km_input: km_input, gas_input: gas_input)
+    }
 }
 
